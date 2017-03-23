bfb42c02c0c092e3814bff914985af98b5d375ed
pocotan001 <pocotan001@gmail.com>
2017-03-23 16:13:11 +0900

    to temporarily off `jsx-a11y/label-has-for` rule

--- jsx-a11y.js
+++ jsx-a11y.js
@@ -4,5 +4,10 @@ module.exports = {
   ],
   extends : [
     'plugin:jsx-a11y/recommended'
-  ]
+  ],
+  rules : {
+    // Waiting for the ['nesting'] rule
+    // https://github.com/evcohen/eslint-plugin-jsx-a11y/issues/51
+    'jsx-a11y/label-has-for' : 'off'
+  }
 };
